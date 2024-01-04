d8a8d8afa8ab16b5771fcf1f4aab404b778e3a3d
lavyatarah <mattlavyy@gmail.com>
2024-01-04 14:13:32 +0300

    updated

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,8 +3,8 @@
 /**
  * binary_tree_is_root - Checks if a given node is a root
  * @node: Pointer to the node to check
- * Return: 1 if node is a root,
- * Otherwise - returns 0
+ * Return: if node is a root -1,
+ * Otherwise - 0.
  *
  * FUNCTIONALITY
  *
